a2630d0cbd22fb7ef179603a8ba9355ac6bf54fc
Rawnak Chourasia <rnkchr0@gmail.com>
2023-05-07 13:48:09 +0530

    Expanded and Image widgets

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.red,
+        appBar: AppBar(
+          title: const Text('Dicee'),
+          backgroundColor: Colors.red,
+        ),
+        body: const DicePage(),
+      ),
+    ),
+  );
+}
+
+class DicePage extends StatelessWidget {
+  const DicePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          flex: 1,
+          child: Image.asset('images/dice1.png'),
+        ),
+        Expanded(
+          flex: 1,
+          child: Image.asset('images/dice1.png'),
+        )
+      ],
+    );
+  }
+}
